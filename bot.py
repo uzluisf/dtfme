4e688c93295976083c41459b1a63a3191ba47bc8
Luis F. Uceta <uzluisf@gmail.com>
2017-08-15 12:37:55 -0400

    Minor fix

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def bot_login():
                           client_secret = client_secret,
                           password = password,
                           user_agent = user_agent, username = username)
-    print("Logged in!")
+
     return reddit 
 
 def run_bot(reddit):
@@ -37,7 +37,7 @@ def run_bot(reddit):
         text = comment.body
         
         if 'dtfm+' in text.lower():
-            print("Word found!")
+            print("\nWord found!")
             whole_string = text.split('+')
             word = whole_string[1]
             word_id = word.lower()
